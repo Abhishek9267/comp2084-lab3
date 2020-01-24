438480ad572f48520a88712990f12a1e1a9b14ce
Abhishek9267 <200424380@student.georgianc.on.ca>
2020-01-23 21:06:24 -0500

    completed lab3

---
+++ PhoneApplication/Controllers/PhoneController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PhoneApplication.Models;
+
+namespace PhoneApplication.Controllers
+{
+    public class PhoneController : Controller
+    {
+        // GET: Phone
+        public ActionResult Index()
+        {
+            var Phonelist = new List<Phone>
+            {
+                new Phone { PhoneID = 0, PhoneName = "Galaxy S10", MSRP=34533, ScreenSize= 43, DateReleased = new DateTime(2020,01,23), Manufacturer = "Samsung" },
+                new Phone { PhoneID = 1, PhoneName = "Oppo f7", MSRP=34533, ScreenSize= 43, DateReleased = new DateTime(2020,01,23), Manufacturer = "oppo"},
+                new Phone { PhoneID = 2, PhoneName = "Model 10", MSRP=34533, ScreenSize= 43, DateReleased  =new DateTime(2020,01,23), Manufacturer = "Apple"}
+            };
+            return View(Phonelist);
+           
+        }
+    }
+}
---
+++ PhoneApplication/Models/Phone.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhoneApplication.Models
+{
+    public class Phone
+    {
+        //shortcut for declairng variables prop tab tab
+        public int PhoneID { get; set; }
+        public double MSRP { get; set; }
+        public DateTime DateReleased { get; set; }
+        public string PhoneName { get; set; }
+        public string Manufacturer { get; set; }
+        public int ScreenSize { get; set; }
+        //string PhoneId, PhoneName, Manufacturer, MSRP;
+        //int ScreenSize;
+        //DateTime
+
+        public Phone()
+        {
+            DateReleased = DateTime.Now;
+            PhoneName = "";
+            Manufacturer = "";
+        }
+    }
+}
